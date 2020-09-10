5297f42e4b2fbe7b887f0f57790f26fad8817173
Filip <off.f.kotelon@gmail.com>
2020-09-10 12:07:03 +0200

    Redux saga

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,13 +7,16 @@ import './header.styles.scss';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
-import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
+import { auth } from '../../firebase/firebase.utils';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
+import { signOutStart } from '../../redux/user/user.actions';
 
-const Header = ({currentUser, hidden}) => (
+
+const Header = ({ currentUser, hidden, signOutStart }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -27,7 +30,7 @@ const Header = ({currentUser, hidden}) => (
       </Link>
       {
         currentUser ?
-        <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
+        <div className="option" onClick={signOutStart}>SIGN OUT</div>
         :
         <Link className="option" to="/signin">
           SIGN IN
@@ -44,4 +47,8 @@ const mapStateToProps = createStructuredSelector({
   hidden: selectCartHidden
 })
 
-export default connect(mapStateToProps)(Header);
+const mapDispatchToProps = dispatch => ({
+  signOutStart: () => dispatch(signOutStart())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+
+import { googleSignInStart, emailSignInStart } from '../../redux/user/user.actions';
 
 class SignIn extends React.Component{
   constructor(props){
@@ -17,17 +19,10 @@ class SignIn extends React.Component{
 
   handleSubmit = async e => {
     e.preventDefault();
+    const { emailSignInStart } = this.props;
+    const { email, password } = this.state;
 
-    const {email, password} = this.state;
-
-    try{
-      await auth.signInWithEmailAndPassword(email, password);
-      this.setState({email: '', password: ''});
-    } catch(error){
-      console.log(error);
-    }
-
-    this.setState({email: '', password: ''});
+    emailSignInStart( email, password );
   }
 
   handleChange = e => {
@@ -37,6 +32,8 @@ class SignIn extends React.Component{
   }
 
   render() {
+    const { googleSignInStart } = this.props;
+
     return(
       <div className="sign-in">
         <h2 className="title">I already have an account</h2>
@@ -62,7 +59,7 @@ class SignIn extends React.Component{
           <div className="buttons">
             <CustomButton type="submit">Sign In</CustomButton>
             <CustomButton 
-              onClick={signInWithGoogle}
+              onClick={googleSignInStart}
               type="button"
               isGoogleSignIn
             >
@@ -75,4 +72,9 @@ class SignIn extends React.Component{
   }
 }
 
-export default SignIn;
+const mapDispatchToProps = dispatch => ({
+  googleSignInStart: () => dispatch(googleSignInStart()),
+  emailSignInStart: ( email, password ) => dispatch(emailSignInStart({ email, password }))
+})
+
+export default connect(null, mapDispatchToProps)(SignIn);
